ab5bbf4b818e624c4887d3c7e07636f8fe2285a7
fess16 <fess61@gmail.com>
2013-04-10 13:50:37 +0300

    add function getBookmark() add function ErrorLog()

--- chrome/content/scripts/overlay.js
+++ chrome/content/scripts/overlay.js
@@ -10,7 +10,7 @@ var GBE =
       Components.
           classes['@mozilla.org/consoleservice;1'].
               getService(Components.interfaces.nsIConsoleService),
-  // список всех закладок (полученный с сервера)
+  // список всех закладок (полученный с сервера) в формате XML
   'm_ganswer' : null,
   // список всех меток (папок)
   'm_labelsArr' : null,
@@ -25,6 +25,85 @@ var GBE =
   // значение поля smh:signature из m_ganswer
   'm_signature' : "",
   // id текущего элемента списка меню закладок (для работы контекстного меню)
-  'currentContextId' : ""
+  'currentContextId' : "",
+
+  /**
+   * поиск информации о закладке по коду (или адресу)
+   * @param  {object} - params информация о закладке
+   * @param  {bool} findByURL = false - признак поиска по адресу
+   * @return {{}}
+   */
+  getBookmark : function(params, findByURL = false)
+  {
+  	// по-умолчанию ищем по коду
+  	var number = 2, value = params.id;
+  	var i;
+  	// если установлен флаг - то по адресу
+  	if (findByURL)
+  	{
+  		number = 1;
+  		value = params.url;
+  	}
+  	// перебираем закладки
+  	for (i = 0; i < GBE.m_bookmarkList.length; i++)
+  	{
+  		// если нашли заполняем поля и выходим
+  		if (GBE.m_bookmarkList[i][number] === value)
+  		{
+  			params.name = GBE.m_bookmarkList[i][0];
+  			params.id = GBE.m_bookmarkList[i][2];
+  			params.url = GBE.m_bookmarkList[i][1];
+  			params.labels = GBE.m_bookmarkList[i][3];
+  			params.notes = GBE.m_bookmarkList[i][4];
+  			return;
+  		}
+  	}
+  	// не нашли - в поле id устанавливаем null 
+  	params.id = null;
+  },
+
+  /**
+   * Вывод отладочных сообщений в консоль
+   * @param {string} s1
+   * @param {string} s2
+   */
+  ErrorLog: function(s1, s2)
+	{
+		GBE.GBLTut_ConsoleService.logStringMessage(s1 + s2);
+	}
+
 
 };
+
+		// var i,
+		// 		count = fessext1.m_bookmarkList.length;
+		// for (i = 0; i < count; i++) 
+		// {
+		// 	if (fessext1.m_bookmarkList[i][1] == cURI)
+		// 	{
+		// 		return true;
+		// 	}
+		// }
+		// return false;
+/*					
+var params = {
+				name : "",
+				id : fessext1.currentContextId.replace("fessext1_",""),
+				url : "",
+				labels : "",
+				notes : "",
+				sig : fessext1.m_signature
+			},
+			i;
+			for (i = 0; i < fessext1.m_bookmarkList.length; i++) 
+			{
+				if (fessext1.m_bookmarkList[i][2] === params.id)
+				{	
+					params.name = fessext1.m_bookmarkList[i][0];
+					// params.id = fessext1.m_bookmarkList[i][2];
+					params.url = fessext1.m_bookmarkList[i][1];
+					params.labels = fessext1.m_bookmarkList[i][3];
+					params.notes = fessext1.m_bookmarkList[i][4];
+					break;
+				}
+			}*/
